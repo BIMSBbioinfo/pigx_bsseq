ac716a416518da8bde0aa99d2500b585dbb893a4
Kasia Wreczycka <katarzyna.wreczycka@mdc-berlin.de>
2017-06-10 12:05:47 +0200

    Update branch; commented out bibliography
    Still have te same error in calculateDiffMeth():
    Error in data.frame(value, row.names = rn, check.names = FALSE, check.rows = FALSE) :
      row names supplied are of the wrong length

--- BSseq_pipeline.py
+++ BSseq_pipeline.py
@@ -297,6 +297,7 @@ rule bismark_genome_preparation:
     shell:
         nice("{BISMARK_GENOME_PREPARATION} {params} {input} > {log} 2>&1 ")
 
+
 # ==========================================================================================
 # post-trimming quality control
 
