c11d90804d73c70251e0c8d172881d18362224ee
Kasia Wreczycka <katarzyna.wreczycka@mdc-berlin.de>
2017-08-11 13:01:34 +0200

    Works besided ../images/Logo_PIGx.png error

--- BSseq_pipeline.py
+++ BSseq_pipeline.py
@@ -99,14 +99,14 @@ OUTPUT_FILES = [
                 # [ expand ( list_files_xmeth( DIR_xmethed, config["SAMPLES"][sampleID]["fastq_name"] )  ) for sampleID in config["SAMPLES"]  ],
 
                 #               ==== rule Bam processing ======
-                [ expand ( bam_processing(METHCALLDIR, config["SAMPLES"][sample]["files"] )  ) for sampleID in config["SAMPLES"]  ], #had to add it to call bam_methCall for diff meth rule
+                [ expand ( bam_processing(METHCALLDIR, config["SAMPLES"][sample]["files"], sample)  ) for sample in config["SAMPLES"]  ], #had to add it to call bam_methCall for diff meth rule
                 
                 #               ==== rule Bam processing ======
                 #[ expand ( bam_processing(METHCALLDIR, config["SAMPLES"][sample]["files"], sample )  ) for sample in config["SAMPLES"]  ], # TODO: had to add this line to call bam_methCall for diff meth rule
 
                 # ==================  FINAL REPORT =========================
                 # TODO: This needs to be editted once we determine what final reports we want to export!
-		[ expand ( Annot(DIR_annot, config["SAMPLES"][sample]["files"], VERSION, config["SAMPLES"][sample]["SampleID"] ) ) for sample in config["SAMPLES"]  ]
+		            [ expand ( Annot(DIR_annot, config["SAMPLES"][sample]["files"], VERSION, config["SAMPLES"][sample]["SampleID"] ) ) for sample in config["SAMPLES"]  ],
                 
 		            # diff meth
 		            [ DIFFMETHDIR+"_".join(x)+".sorted_diffmeth.nb.html" for x in config["DIFF_METH"]  ],
