a9bd2a463fe9b4b57503dfe72247eebedaf5bc23
Alexander Blume <alex.gos90@gmail.com>
2022-09-15 21:28:33 +0200

    Align_bwameth_rules: format with snakefmt
    Signed-off-by: Alexander Blume <alex.gos90@gmail.com>

--- rules/Align_bwameth_rules.py
+++ rules/Align_bwameth_rules.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-### This file was originally taken and modified from  
+### This file was originally taken and modified from
 ### https://github.com/katwre/makeWGBSnake/blob/master/Rules/Align_bwameth_rules.py
 
 # WGBS pipeline
@@ -28,78 +28,90 @@
 
 # # # ==========================================================================================
 # # # Mapping bisulfite reads with bwa-meth:
-# # 
+# #
 
 # Notes:
 # bwa meth paper https://arxiv.org/pdf/1401.1129.pdf
 # https://www.biostars.org/p/93726/
 # bwa meth and snp http://seqanswers.com/forums/showthread.php?t=40562
 
-# 
+#
 # # ==========================================================================================
 # # Generate methyl-converted version of the reference genome:
-#       
+#
+
 
 rule bwameth_genome_preparation:
     input:
-        ancient(GENOMEFILE)
+        ancient(GENOMEFILE),
     output:
-        protected(GENOMEFILE+".bwameth.c2t.sa"),
-        protected(GENOMEFILE+".bwameth.c2t.amb"),
-        protected(GENOMEFILE+".bwameth.c2t.ann"),
-        protected(GENOMEFILE+".bwameth.c2t.pac"),
-        protected(GENOMEFILE+".bwameth.c2t.bwt"),
-        protected(GENOMEFILE+".bwameth.c2t")
+        protected(GENOMEFILE + ".bwameth.c2t.sa"),
+        protected(GENOMEFILE + ".bwameth.c2t.amb"),
+        protected(GENOMEFILE + ".bwameth.c2t.ann"),
+        protected(GENOMEFILE + ".bwameth.c2t.pac"),
+        protected(GENOMEFILE + ".bwameth.c2t.bwt"),
+        protected(GENOMEFILE + ".bwameth.c2t"),
     log:
-        os.path.join(GENOMEPATH,'bwameth_genome_preparation.output_'+ASSEMBLY+'.log')
-    message: fmt("Converting {ASSEMBLY} Genome into Bisulfite analogue with bwa-meth")
+        os.path.join(
+            GENOMEPATH, "bwameth_genome_preparation.output_" + ASSEMBLY + ".log"
+        ),
+    message:
+        fmt("Converting {ASSEMBLY} Genome into Bisulfite analogue with bwa-meth")
     shell:
-        nice("bwameth", ["index {input}"],"{log}")
+        nice("bwameth", ["index {input}"], "{log}")
+
 
 rule bwameth_touch_index:
     input:
-        GENOMEFILE+".bwameth.c2t.sa",
-        GENOMEFILE+".bwameth.c2t.amb",
-        GENOMEFILE+".bwameth.c2t.ann",
-        GENOMEFILE+".bwameth.c2t.pac",
-        GENOMEFILE+".bwameth.c2t.bwt"
+        GENOMEFILE + ".bwameth.c2t.sa",
+        GENOMEFILE + ".bwameth.c2t.amb",
+        GENOMEFILE + ".bwameth.c2t.ann",
+        GENOMEFILE + ".bwameth.c2t.pac",
+        GENOMEFILE + ".bwameth.c2t.bwt",
     output:
-        GENOMEFILE+".bwameth.c2t_was.touched"
-    message: fmt("Update timestamp for {ASSEMBLY} Genome Index")
+        GENOMEFILE + ".bwameth.c2t_was.touched",
+    message:
+        fmt("Update timestamp for {ASSEMBLY} Genome Index")
     shell:
         "sleep 60; touch {input};touch {output}"
 
 
 # # ==========================================================================================
 # # Align whole data sets
-#    
+#
 
 
 def bwameth_input(sample):
-    files = list_files_TG(samplesheet(sample,'files'),sample, '')
-    return(files)
+    files = list_files_TG(samplesheet(sample, "files"), sample, "")
+    return files
+
 
 rule bwameth_align_trimmed:
     input:
         rules.bwameth_touch_index.output,
-        index = rules.bwameth_genome_preparation.output,
-        files = lambda wc: bwameth_input(wc.sample)
+        index=rules.bwameth_genome_preparation.output,
+        files=lambda wc: bwameth_input(wc.sample),
     output:
-        bam = DIR_mapped+"{sample}.bwameth.bam"
+        bam=DIR_mapped + "{sample}.bwameth.bam",
     params:
-      # bwa-meth parameters
-        threads = config['execution']['rules']['bwameth_align_trimmed']['threads']
+        # bwa-meth parameters
+        threads=config["execution"]["rules"]["bwameth_align_trimmed"]["threads"],
     log:
-        DIR_mapped+"{sample}_bwameth_mapping.log"
-    message: fmt("Mapping reads to genome using bwa-meth for sample {wildcards.sample}.")
+        DIR_mapped + "{sample}_bwameth_mapping.log",
+    message:
+        fmt("Mapping reads to genome using bwa-meth for sample {wildcards.sample}.")
     shell:
-      nice("bwameth",
-      ["--reference {GENOMEFILE}",
-      "-t {params.threads}",
-      "{input.files}","2>> {log}",
-      "|", 
-      tool('samtools'), 
-      "view -bS - -o {output.bam}",
-      "2>> {log}"],
-      "{log}")
-
+        nice(
+            "bwameth",
+            [
+                "--reference {GENOMEFILE}",
+                "-t {params.threads}",
+                "{input.files}",
+                "2>> {log}",
+                "|",
+                tool("samtools"),
+                "view -bS - -o {output.bam}",
+                "2>> {log}",
+            ],
+            "{log}",
+        )
