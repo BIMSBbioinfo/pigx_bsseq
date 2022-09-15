f0dd9279b761852ce32756130775021b40f14b7c
Alexander Blume <alex.gos90@gmail.com>
2022-09-15 21:28:33 +0200

    multiqc: apply formatter
    Signed-off-by: Alexander Blume <alex.gos90@gmail.com>

--- rules/multiqc.py
+++ rules/multiqc.py
@@ -1,9 +1,10 @@
 # ---------------------------------------------------------------------------- #
 
+
 def multiqc_files(branch):
     files = files_for_sample(list_files_rawQC)
     files += files_for_sample(list_files_TG)
-    files += files_for_sample(list_files_posttrim_QC) 
+    files += files_for_sample(list_files_posttrim_QC)
     if branch == "bismark":
         files += files_for_sample(list_files_bismark)
         files += files_for_sample(list_files_methyldackel_mbias_bismark)
@@ -13,19 +14,38 @@ def multiqc_files(branch):
         files += files_for_sample(list_files_markdup)
         files += files_for_sample(list_files_methyldackel_mbias_bwameth)
     files = list(chain.from_iterable(files))
-    return(files)
+    return files
+
 
 rule multiqc:
     input:
-        files = lambda wc: multiqc_files(wc.branch)
+        files=lambda wc: multiqc_files(wc.branch),
     output:
-        os.path.join(DIR_final,"multiqc", "{branch}_multiqc_report.html")
+        os.path.join(
+            DIR_final,
+            "multiqc",
+            "{branch}_multiqc_report.html",
+        ),
     params:
-        trim_galore = DIR_trimmed,
-        raw_qc = DIR_rawqc,
-        posttrim_qc = DIR_posttrim_QC
+        trim_galore=DIR_trimmed,
+        raw_qc=DIR_rawqc,
+        posttrim_qc=DIR_posttrim_QC,
     log:
-        os.path.join(DIR_final,"multiqc", "{branch}_multiqc.log")
-    message: fmt("Generating multi-sample QC report for {wildcards.branch} branch.")
+        os.path.join(
+            DIR_final,
+            "multiqc",
+            "{branch}_multiqc.log",
+        ),
+    message:
+        fmt("Generating multi-sample QC report for {wildcards.branch} branch.")
     shell:
-      nice("multiqc",["-f","-n {output}","{input.files}","{params}"],"{log}")
+        nice(
+            "multiqc",
+            [
+                "-f",
+                "-n {output}",
+                "{input.files}",
+                "{params}",
+            ],
+            "{log}",
+        )
